4a78cb71ba0ed7358a21ecdbd428ebec91dc73a4
Phan Nika <phannika237@gmail.com>
2024-11-21 17:49:49 +0700

    project tax system

---
+++ TaxSystem/frmTaxSystem.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace TaxSystem
+{
+    partial class frmTaxSystem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtGrossSalary = new System.Windows.Forms.TextBox();
+            this.txtTax = new System.Windows.Forms.TextBox();
+            this.txtNetSalary = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(-2, -4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(575, 60);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Salary Tax Application";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Gross-Salary:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 33);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Net-Salary:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 33);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Tax:";
+            // 
+            // txtGrossSalary
+            // 
+            this.txtGrossSalary.Location = new System.Drawing.Point(182, 78);
+            this.txtGrossSalary.Name = "txtGrossSalary";
+            this.txtGrossSalary.Size = new System.Drawing.Size(299, 40);
+            this.txtGrossSalary.TabIndex = 4;
+            // 
+            // txtTax
+            // 
+            this.txtTax.Location = new System.Drawing.Point(182, 124);
+            this.txtTax.Name = "txtTax";
+            this.txtTax.Size = new System.Drawing.Size(299, 40);
+            this.txtTax.TabIndex = 5;
+            // 
+            // txtNetSalary
+            // 
+            this.txtNetSalary.Location = new System.Drawing.Point(182, 182);
+            this.txtNetSalary.Name = "txtNetSalary";
+            this.txtNetSalary.Size = new System.Drawing.Size(299, 40);
+            this.txtNetSalary.TabIndex = 6;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(182, 302);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(129, 36);
+            this.btnCalculate.TabIndex = 7;
+            this.btnCalculate.Text = "Caculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(352, 302);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(129, 36);
+            this.btnCancel.TabIndex = 8;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // frmTaxSystem
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(543, 437);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtNetSalary);
+            this.Controls.Add(this.txtTax);
+            this.Controls.Add(this.txtGrossSalary);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Khmer OS Siemreap", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "frmTaxSystem";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmTaxSystem";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtGrossSalary;
+        private System.Windows.Forms.TextBox txtTax;
+        private System.Windows.Forms.TextBox txtNetSalary;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ TaxSystem/frmTaxSystem.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaxSystem
+{
+    public partial class frmTaxSystem : Form
+    {
+        public frmTaxSystem()
+        {
+            InitializeComponent();
+        }
+        private double CalculateTax(double income)
+        {
+            double tax = 0;
+
+            if (income <= 1500000)
+            {
+                tax = 0;
+            }
+            else if (income <= 2000000)
+            {
+                tax = (income - 1500000) * 0.05;
+            }
+            else if (income <= 8500000)
+            {
+                tax = (500000 * 0.05) + ((income - 2000000) * 0.10);
+            }
+            else if (income <= 12500000)
+            {
+                tax = (500000 * 0.05) + (6500000 * 0.10) + ((income - 8500000) * 0.15);
+            }
+            else
+            {
+                tax = (500000 * 0.05) + (6500000 * 0.10) + (4000000 * 0.15) + ((income - 12500000) * 0.20);
+            }
+
+            return tax;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double grossSalary;
+            if (double.TryParse(txtGrossSalary.Text, out grossSalary))
+            {
+                double tax = CalculateTax(grossSalary);
+                double netSalary = grossSalary - tax;
+
+                txtTax.Text = tax.ToString("F2");
+                txtNetSalary.Text = netSalary.ToString("F2");
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid gross salary.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            txtGrossSalary.Clear();
+            txtTax.Clear();
+            txtNetSalary.Clear();
+        }
+    }
+}
